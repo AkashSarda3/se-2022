c5054f7238db8920bcfc1b615dfd3fc4036e200f
arvindsrinivas1 <arvindsrinivas96@gmail.com>
2022-08-26 19:49:38 -0400

    hw1-pylint changes + fixed readme filename in setup

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from distutils.core import setup
 setup(
     name='SEHW1',
     version='0.1dev',
-    packages=['code',],
+    packages=['code', ],
     license='MIT',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
     install_requires=[
         'pytest'
     ]
